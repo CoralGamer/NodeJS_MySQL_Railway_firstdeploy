effdaff3f02f9e1fffd44f960051f6e398b61923
Nicolas A. Romero <nicolas.romeroc@hotmail.com>
2024-11-16 19:39:47 -0500

    corregido error con conexion a las bases de datos en el deploy

--- src/config.js
+++ src/config.js
@@ -5,3 +5,10 @@ export const DB_USER = process.env.MYSQLUSER || 'root'
 export const DB_PASSWORD = process.env.MYSQLPASSWORD || ''
 export const DB_NAME = process.env.MYSQLDATABASE || 'usersdb'
 export const DB_PORT = process.env.MYSQLPORT || 3306
+
+console.log('Database Configuration:', {
+    host: DB_HOST,
+    user: DB_USER,
+    database: DB_NAME,
+    port: DB_PORT
+});
--- src/db.js
+++ src/db.js
@@ -1,16 +1,12 @@
 import {createPool} from 'mysql2/promise'
-import {
-    DB_HOST,
-    DB_NAME,
-    DB_PASSWORD,
-    DB_USER,
-    DB_PORT
-} from './config.js'
 
 export const pool = createPool({
-    user: DB_USER,
-    password: DB_PASSWORD,
-    host: DB_HOST,
-    port: DB_PORT,
-    database: DB_NAME
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 })
